b814557eb02e12128644354107a5e73e83e739f4
longsl <583297550@qq.com>
2023-08-09 10:17:53 +0800

    [+]add build script

---
+++ Assets/Editor/GameBuilder.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class GameBuilder
+{
+    [MenuItem("Build/Build Windows")]
+    public static void BuildWindowsBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "build/Windows/SuperAwesomeGame.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport buildPlayer = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = buildPlayer.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+    [MenuItem("Build/Build WebGL")]
+    public static void BuildWebGLBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "build/WebGL";
+        buildPlayerOptions.target = BuildTarget.WebGL;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport buildPlayer = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = buildPlayer.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
